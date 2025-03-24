e2d9e9869c5c1ada5b2c15fef5bd6c97ee9bb9b3
fminondo1 <79666284+fminondo1@users.noreply.github.com>
2025-03-24 02:51:14 +0000

    Add startup.sh for Azure deployment

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /tmp/8dd6a735c752dff/antenv/bin/activate
-echo "Starting Flask app with gunicorn on $PORT..."
-gunicorn --bind=0.0.0.0:$PORT --timeout 600 app:app
+echo "Starting Flask app with gunicorn on \$PORT..."
+gunicorn --bind=0.0.0.0:\$PORT --timeout 600 app:app
